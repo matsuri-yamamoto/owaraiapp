9949caccd5e6826166377553468d37a2a34c710a
rino.yamamoto <rino.matsushima@gmail.com>
2022-03-20 18:55:47 +0900

    Initial Commit

---
+++ owaraimemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  owaraimemo
+//
+//  Created by 山本梨野 on 2022/03/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
